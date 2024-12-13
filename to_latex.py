40995c25bbda3c1915b64b02a8fa176c7a5b0384
Tobbe821 <34543773+Globose@users.noreply.github.com>
2024-12-13 20:39:30 +0100

    no

--- to_latex.py
+++ to_latex.py
@@ -1,7 +1,8 @@
 import pandas as pd
+import os
 
-def main():
-    A = pd.read_csv('df_scores_2.csv')
+def print_table(file):
+    A = pd.read_csv(file)
     A = A.to_latex(index=False, float_format="%.4f")
     L = [x for x in A.split('\n')]
     
@@ -15,14 +16,31 @@ def main():
     L[1] = "\\hline"
     L[3] = "\\hline"
     L[-3] = "\\hline"
-    L.insert(-5, "\\hline")
+    L.insert(14-len(L), "\\hline")
 
-    L.insert(0, "\\begin{table}[htbp!]")
+    L.insert(0, "\\begin{table}[H]")
     L.insert(1, "\\centering\\scriptsize")
+    L.insert(2, "\\caption{"+file[:-4].replace('_', ' ')+"}")
+    L.insert(3, "\\label{"+file[:-4]+"}")
     L[-1] = "\\end{table}"
 
-    for l in L:
-        print(l)
+    L.insert(0, "\\newcommand{\\"+ file[:-4].replace('_', '') + "}{")
+    L.append("}")
+
+    return L
+
+def main():
+    files = ["df_time.csv", "df_acc.csv", "df_f.csv", "friedman_fit_time.csv", "friedman_accuracy_score.csv", "friedman_f_score.csv"]
+    document = []
+    for file in files:
+        document.extend(print_table(file))
+
+    with open('tables.txt', 'w') as file:
+        for line in document:
+            file.write(line)
+            file.write("\n")
+
+    
 
 if __name__ == '__main__':
     main()
